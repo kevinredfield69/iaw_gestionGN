7a2132a988f085a0fbe019bf7d46585f2fe568bc
root <root@python5-produccion.novalocal>
2018-11-21 15:58:46 +0000

    Editando fichero principal de gestion

--- gestion/wsgi.py
+++ gestion/wsgi.py
@@ -7,11 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gestion.settings")
 
+sys.path.append('/var/www/html/iaw_gestionGN')
+
+#sys.path.insert(0, '/var/www/html/iaw_gestionGN')
+activate_this = '/home/debian/env/django/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
 
 application = get_wsgi_application()
-
